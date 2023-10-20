7301c3b860142a79346f6fade4d9a264f3aee388
Captin <a@a.com>
2023-10-20 11:57:18 +0100

    first commit

---
+++ src/PeopleTeamPink/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("John", "Boris", LocalDate.of(1998, 05, 21));
+        Person person2 = new Person("Ali", "Ahmed", LocalDate.of(1995, 04, 21));
+        Person person3 = new Person("John", "Jake", LocalDate.of(1062, 01, 28));
+
+        person1.getFormatedString();
+
+    }
+}
---
+++ src/PeopleTeamPink/src/main/java/org/example/Person.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Person {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getAge() {
+        return Period.between(this.dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    public void getFormatedString() {
+        System.out.printf("First Name: %s %n", this.firstName );
+        System.out.printf("Last Name: %s %n", this.lastName );
+
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy ");
+        System.out.printf("DOB: %s %n", this.dateOfBirth.format(myFormatObj) );
+
+        System.out.printf("Age: %d %n", this.getAge() );
+        System.out.printf("Number of People: %d %n", personCount );
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    private String firstName, lastName;
+    private LocalDate dateOfBirth;
+    private static int personCount;
+
+    public Person (String fn, String ln, LocalDate dob){
+        firstName = fn;
+        lastName = ln;
+        dateOfBirth = dob;
+        personCount++;
+    }
+
+
+
+
+}
