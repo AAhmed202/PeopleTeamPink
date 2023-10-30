4adde454ca3326be7b4337a1ba8061ac563360e7
Katie <email@email.com>
2023-10-30 12:12:17 +0000

    code cleanup

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -1,12 +1,7 @@
 package org.example;
 import org.junit.*;
-import org.junit.runners.MethodSorters;
 import static org.junit.Assert.assertEquals;
 import java.time.LocalDate;
-
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//Tests are ordered to ensure correct personCount is checked for
-
 
 public class PersonTest {
     Person person1 = new Person("John", "Boris", LocalDate.of(1998, 05, 21));
@@ -18,7 +13,6 @@ public class PersonTest {
 
     @Test
     public void getAge() {
-        //Person person1 = new Person("Steve", "Smith", LocalDate.of(1998, 03, 12));
         int expectedAge = 25;
         int generatedAge = person1.getAge();
         assertEquals(expectedAge,generatedAge);
@@ -26,7 +20,6 @@ public class PersonTest {
 
     @Test
     public void getFormattedString() {
-        //Person person2 = new Person("John", "Boris", LocalDate.of(1998, 05, 21));
         String expectedString = "First Name: John\nLast Name: Boris\nDOB: 21-05-1998\nAge: 25\nNumber of People: 1";
         String formattedString = person1.getFormattedString();
         assertEquals(expectedString,formattedString);
@@ -34,7 +27,6 @@ public class PersonTest {
 
     @Test
     public void getPersonCount() {
-        //Person person3 = new Person("Steve", "Bruce", LocalDate.of(1986,9, 11));
         int expectedCount = 1;
         int generatedCount = person1.getCount();
         assertEquals(expectedCount,generatedCount);
